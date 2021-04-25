07c9cba873b347f08811fea2ea06142974d6ad3a
RickeyRM <97_roman_97@mail.ru>
2021-04-25 15:20:25 +0300

    модальное окно

--- JS/app.js
+++ JS/app.js
@@ -1,45 +1,56 @@
 $(document).ready(function () {
 
-    $('.team__name').on('click', e =>{
-        
-        let post = $(e.currentTarget).next();
+    $('.form').submit(e =>{
+        e.preventDefault();
 
-        post.slideToggle();
-
-    });
-
-    $('.product__list').slick({
-        infinite: false,
-        prevArrow: $('.arrow--left'),
-        nextArrow: $('.arrow--right'),
+        $.fancybox.open({
+            src: ".modal",
+            type: "inline"
+        })
     });
 
 });
 
 
 
-(function openMenu() {
-    const menu = document.querySelector('.menu');
-    const menuBtn = document.querySelector('.burger');
 
-    menuBtn.addEventListener('click', function () {
-        menu.classList.toggle('menu--active');
-        menuBtn.classList.toggle('active');
+const myForm = document.querySelector('.form');
+const sendForm = document.querySelector('.form__btn');
 
-        if (menu.classList.contains('menu--active')){
-            document.querySelector('.wrapper').style.overflow =  `hidden`;
-            document.querySelector('.wrapper').style.height =  `100vh`;
-        } else {
-            document.querySelector('.wrapper').style.overflow = `scroll`;
-        }
-    });
+sendForm.addEventListener('click', e =>{
+    e.preventDefault();
 
-    menu.addEventListener('click', function (e) {
-        if (menu.classList.contains('menu--active') && e.target.classList.contains('menu__item-link')){
-            menu.classList.toggle('menu--active');
-            menuBtn.classList.toggle('active');
-            document.querySelector('.wrapper').style.overflow = `scroll`;
-        }
-    })
+    if(validateForm(myForm)){
+        console.log('бибип');
+    }
+});
 
-})();
+function validateForm(form){
+    let valid = true;
+
+    if(!validateChecked(form.elements.name)){
+        valid = false;
+    };
+    if(!validateChecked(form.elements.tel)){
+        valid = false;
+    };
+    if(!validateChecked(form.elements.street)){
+        valid = false;
+    };
+    if(!validateChecked(form.elements.house)){
+        valid = false;
+    };
+    if(!validateChecked(form.elements.flat)){
+        valid = false;
+    };
+    return valid;
+};
+function validateChecked(checked){
+    if(!checked.checkValidity()){
+        checked.style.border = 'rem(3px) solid red';
+        return false;
+    } else {
+        checked.style.border = 'rem(3px) solid #DEE2BD';
+        return true;
+    }
+};
