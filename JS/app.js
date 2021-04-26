f22c8169ec7469a9a93837dd969a8b7b40b9cb5f
RickeyRM <97_roman_97@mail.ru>
2021-04-26 18:28:57 +0300

    на предварительную проверку

--- JS/app.js
+++ JS/app.js
@@ -1,56 +1,93 @@
 $(document).ready(function () {
 
-    $('.form').submit(e =>{
+    const myForm = document.querySelector('.form');
+    const sendForm = document.querySelector('.form__btn');
+    
+    sendForm.addEventListener('click', e =>{
         e.preventDefault();
 
-        $.fancybox.open({
-            src: ".modal",
-            type: "inline"
-        })
+        
+        const name = $(myForm).find("[name='name']");
+        const phone = $(myForm).find("[name='phone']");
+        const street = $(myForm).find("[name='street']");
+        const to = $(myForm).find("[name='to']");
+        const house = $(myForm).find("[name='house']");
+        const flat = $(myForm).find("[name='flat']");
+
+        [name, phone, street, to, flat, house].forEach((field) =>{
+            field.removeClass('false');
+            if(field.val().trim() === ''){
+                field.addClass('false');
+            }
+        });
+
+        if(validateForm(myForm)){
+
+            const data ={
+                name: myForm.elements.name.value,
+                phone: myForm.elements.phone.value,
+                comment: myForm.elements.comment.value,
+                to: myForm.elements.to.value,  
+            };
+
+            console.log(data);
+            const xhr = new XMLHttpRequest();
+            xhr.responseType = 'json';
+            xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+            xhr.send(JSON.stringify(data));
+            xhr.addEventListener('load', () =>{
+                // if(xhr.response.status){
+                //     console.log('ok');
+                // } else{
+                //     console.log('ne ok');
+                // }
+            });
+        }
     });
+    
+    function validateForm(form){
+        let valid = true;
+    
+        if(!validateChecked(form.elements.name)){
+            valid = false;
+        };
+        if(!validateChecked(form.elements.phone)){
+            valid = false;
+        };
+        if(!validateChecked(form.elements.to)){
+            valid = false;
+        };
+        if(!validateChecked(form.elements.street)){
+            valid = false;
+        };
+        if(!validateChecked(form.elements.house)){
+            valid = false;
+        };
+        if(!validateChecked(form.elements.flat)){
+            valid = false;
+        };
+        return valid;
+    };
+    function validateChecked(checked){
+        $.fancybox.open({
+            src: "#modal",
+            type: "inline"
+        });
+
+        $('.btn__modal-js').click( e =>{
+            e.preventDefault();
+    
+            $.fancybox.close();
+        });
+        if(!checked.checkValidity()){
+           $('.modal__title').text('Данные не отправлены').next().text('Заполните поля');
+            return false;
+        } else {
+            $('.modal__title').text('Данные отправлены').next().text('');
+            return true;
+        }
+    }
+
 
 });
-
-
-
-
-const myForm = document.querySelector('.form');
-const sendForm = document.querySelector('.form__btn');
-
-sendForm.addEventListener('click', e =>{
-    e.preventDefault();
-
-    if(validateForm(myForm)){
-        console.log('бибип');
-    }
-});
-
-function validateForm(form){
-    let valid = true;
-
-    if(!validateChecked(form.elements.name)){
-        valid = false;
-    };
-    if(!validateChecked(form.elements.tel)){
-        valid = false;
-    };
-    if(!validateChecked(form.elements.street)){
-        valid = false;
-    };
-    if(!validateChecked(form.elements.house)){
-        valid = false;
-    };
-    if(!validateChecked(form.elements.flat)){
-        valid = false;
-    };
-    return valid;
-};
-function validateChecked(checked){
-    if(!checked.checkValidity()){
-        checked.style.border = 'rem(3px) solid red';
-        return false;
-    } else {
-        checked.style.border = 'rem(3px) solid #DEE2BD';
-        return true;
-    }
-};
+    
